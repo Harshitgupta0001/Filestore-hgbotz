d6ca9cbab89703caba3d6bd9407a9fbe1da4ba10
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-12-05 18:21:00 +0530

    Update dbusers.py

--- plugins/dbusers.py
+++ plugins/dbusers.py
@@ -1,4 +1,3 @@
-
 import motor.motor_asyncio
 from config import DB_NAME, DB_URI
 
@@ -14,11 +13,11 @@ class Database:
         self.col = self.db.users
         self.grp = self.db.groups
 
-
     def new_user(self, id, name):
         return dict(
-            id = id,
-            name = name,
+            id=id,
+            name=name,
+            is_authorized=False,  # Default to unauthorized
             ban_status=dict(
                 is_banned=False,
                 ban_reason="",
@@ -27,34 +26,38 @@ class Database:
 
     def new_group(self, id, title):
         return dict(
-            id = id,
-            title = title,
+            id=id,
+            title=title,
             chat_status=dict(
                 is_disabled=False,
                 reason="",
             ),
         )
 
-    
     async def add_user(self, id, name):
         user = self.new_user(id, name)
         await self.col.insert_one(user)
 
-    
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'id':int(id)})
+        user = await self.col.find_one({'id': int(id)})
         return bool(user)
 
+    async def is_user_authorized(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get("is_authorized", False) if user else False
+
+    async def authorize_user(self, id):
+        await self.col.update_one({'id': int(id)}, {'$set': {'is_authorized': True}})
+
+    async def unauthorize_user(self, id):
+        await self.col.update_one({'id': int(id)}, {'$set': {'is_authorized': False}})
 
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
 
-    
     async def get_all_users(self):
         return self.col.find({})
-
-
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
